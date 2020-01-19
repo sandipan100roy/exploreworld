089ff5320018dea31e58d01a1eb5c022d2aec58d
sandipan100roy <sandipan.100roy@gmail.com>
2020-01-19 18:54:08 +0530

    Added code for category details page

--- src/app/components/categories/category-details/category-details.component.ts
+++ src/app/components/categories/category-details/category-details.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
+import { CategoriesService } from '../../../services/categories.service';
+
 @Component({
   selector: 'app-category-details',
   templateUrl: './category-details.component.html',
@@ -9,10 +11,20 @@ import { ActivatedRoute } from '@angular/router';
 export class CategoryDetailsComponent implements OnInit {
   
   categorySelected;
-  constructor(private activatedRoute : ActivatedRoute) { }
+  categoryList;
+  constructor(private activatedRoute : ActivatedRoute, 
+  	private categoriesService : CategoriesService) { }
 
   ngOnInit() {
   	this.categorySelected = this.activatedRoute.snapshot.params['category'];
+  	this.categoriesService.getCategoryListDate(this.categorySelected)
+  	.subscribe((data) => {
+  		//console.log(data);
+      this.categoryList = data;
+  	},
+    err =>{
+      console.log("Error from Server : ",err);
+    });
   }
 
 }
--- src/app/services/categories.service.ts
+++ src/app/services/categories.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+import { map } from 'rxjs/operators';
+
 @Injectable()
 
 export class CategoriesService{
@@ -12,5 +14,22 @@ export class CategoriesService{
 	getCategoriesData(){
 		//console.log("Frm");
 		return this.http.get('https://exploreworld-2711e.firebaseio.com/.json');
+	}
+
+	getCategoryListDate(id){
+		let filteredresponseData;
+		return this.http.get('https://exploreworld-2711e.firebaseio.com/.json').pipe(
+			//filter(responseData => responseData == id),
+			map(responseData => {
+				if(Array.isArray(responseData)){
+					filteredresponseData = responseData.filter(
+						data => data.cid == 4
+					);
+				}
+				console.log(filteredresponseData);
+				return filteredresponseData[0].categorylist;
+			},
+          		error => error)
+		);
 	}
 }
